0e88fb5434ac313d303f36837d251ffadb8c584f
Nikita Bushuev <nikitabusuev801@gmail.com>
2026-03-13 21:50:07 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import MainLayout from "./layouts/MainLayout.jsx";
 import Form from "./components/Form/Form.jsx";
-import Gallery from "./components/Gallery/Gallery.jsx";
 
 export default function App() {
   return (
     <MainLayout>
       <Form />
-      <Gallery />
     </MainLayout>
   );
 }
